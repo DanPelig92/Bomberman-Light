2b53779a3dc6cb089fcf36d8bef97d7a03a3f06f
Dan <you@example.com>
2022-11-16 14:49:21 +0100

    Funziona.

--- script.js
+++ script.js
@@ -27,31 +27,34 @@ let cellaDodici = document.getElementById("12")
 let cellaTredici = document.getElementById("13")
 let cellaQuattordici = document.getElementById("14")
 let cellaQuindici = document.getElementById("15")
-cellaZero.addEventListener("click", function() {clickCella(cellaZero)}, false)
-cellaUno.addEventListener("click", function() {clickCella(cellaUno)}, false)
-cellaDue.addEventListener("click", function() {clickCella(cellaDue)}, false)
-cellaTre.addEventListener("click", function() {clickCella(cellaTre)}, false)
-cellaQuattro.addEventListener("click", function() {clickCella(cellaQuattro)}, false)
-cellaCinque.addEventListener("click", function() {clickCella(cellaCinque)}, false)
-cellaSei.addEventListener("click", function() {clickCella(cellaSei)}, false)
-cellaSette.addEventListener("click", function() {clickCella(cellaSette)}, false)
-cellaOtto.addEventListener("click", function() {clickCella(cellaOtto)}, false)
-cellaNove.addEventListener("click", function() {clickCella(cellaNove)}, false)
-cellaDieci.addEventListener("click", function() {clickCella(cellaDieci)}, false)
-cellaUndici.addEventListener("click", function() {clickCella(cellaUndici)}, false)
-cellaDodici.addEventListener("click", function() {clickCella(cellaDodici)}, false)
-cellaTredici.addEventListener("click", function() {clickCella(cellaTredici)}, false)
-cellaQuattordici.addEventListener("click", function() {clickCella(cellaQuattordici)}, false)
-cellaQuindici.addEventListener("click", function() {clickCella(cellaQuindici)}, false)
+cellaZero.addEventListener("click", function() {clickCella(cellaZero)}, {once:true})
+cellaUno.addEventListener("click", function() {clickCella(cellaUno)}, {once:true})
+cellaDue.addEventListener("click", function() {clickCella(cellaDue)}, {once:true})
+cellaTre.addEventListener("click", function() {clickCella(cellaTre)}, {once:true})
+cellaQuattro.addEventListener("click", function() {clickCella(cellaQuattro)}, {once:true})
+cellaCinque.addEventListener("click", function() {clickCella(cellaCinque)}, {once:true})
+cellaSei.addEventListener("click", function() {clickCella(cellaSei)}, {once:true})
+cellaSette.addEventListener("click", function() {clickCella(cellaSette)}, {once:true})
+cellaOtto.addEventListener("click", function() {clickCella(cellaOtto)}, {once:true})
+cellaNove.addEventListener("click", function() {clickCella(cellaNove)}, {once:true})
+cellaDieci.addEventListener("click", function() {clickCella(cellaDieci)}, {once:true})
+cellaUndici.addEventListener("click", function() {clickCella(cellaUndici)}, {once:true})
+cellaDodici.addEventListener("click", function() {clickCella(cellaDodici)}, {once:true})
+cellaTredici.addEventListener("click", function() {clickCella(cellaTredici)}, {once:true})
+cellaQuattordici.addEventListener("click", function() {clickCella(cellaQuattordici)}, {once:true})
+cellaQuindici.addEventListener("click", function() {clickCella(cellaQuindici)}, {once:true})
 
 function clickCella(cella) {
     document.getElementById(cella.id).textContent = grid[cella.id]
+    console.log(grid)
     if (grid[cella.id]== "Boom") {
-        messaggioBenvenuto.textContent = "Hai perso, stronzo";
-    } else {
+        alert("BOOM! Sei esploso, hai perso");
+        window.location.reload();
+    } else { 
         counter++
     }
     if (counter==14){
-        messaggioBenvenuto.textContent = "Hai vinto, stronzo";
+        alert("Hai vinto! Complimenti!");
+        window.location.reload();
     }
 }
